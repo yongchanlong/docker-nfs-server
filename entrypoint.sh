0478ecbf0d190a89d1bbc50b90e96da3e1a0732b
yongchanlong <33020278+yongchanlong@users.noreply.github.com>
2018-03-20 15:44:25 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 set -ex
 
+if [ ! -d "/storage" ]; then
+  mkdir /storage
+fi
+
+if [ ! -d "/storage/docker-nfs-server" ]; then
+  mkdir /storage/docker-nfs-server
+fi
+
+file="/storage/docker-nfs-server/exports"
+if [ ! -f "$file" ]; then
+  echo "
+  /storage *(rw,fsid=0,insecure,no_root_squash,no_subtree_check,sync)
+  " > /storage/docker-nfs-server/exports
+fi
+
+file="/etc/exports"
+if [ -f "$file" ]; then
+  rm /etc/exports
+fi
+
+ln /storage/docker-nfs-server/exports /etc/exports
+    
 mount -t nfsd nfsd /proc/fs/nfsd
 # Fixed nlockmgr port
 echo 'fs.nfs.nlm_tcpport=32768' >> /etc/sysctl.conf
